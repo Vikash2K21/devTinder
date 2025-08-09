1fb61b37e8c9af297ac5600c92cf027a64d295d2
Vikash Kumar <vikashmrh1@gmail.com>
2025-08-09 22:06:04 +0530

    exploring get, post ,delete

--- src/app.js
+++ src/app.js
@@ -1,18 +1,33 @@
 const express = require("express");
-
 const app = express();
 
+app.use("/user",(req,res) =>{
+    res.send("HHHHHHHHHH");
+})
+
+//get method
+
+app.get("/user", (req,res) =>{
+    res.send({firstname : "vikash", lastname: "kumar"});
+});
+
+//post method 
+
+app.post("/user",(req,res) =>{
+    res.send("Save data successfully in database");
+})
+
+//delete method
+
+app.delete("/user",(req,res) =>{
+    res.send("deleted successfully");
+})
+
+//This will match all the http methods api calls to /test
 app.use((req,res) => {
     res.send("Namaste nodejs!");
-})
+});
 
-app.use("/",(req,res) => {
-    res.send("Hello Vikash!");
-})
-
-app.use("/test",(req,res) => {
-    res.send("Hello from the server!");
-})
 
 app.listen(3000, () => {
     console.log("server is running on port 3000");
--- src/homework.md
+++ src/homework.md
@@ -8,3 +8,12 @@ write a request and update scripts inside package json
 what are dependencies
 what is the use of "-g" while npm install 
 difference between caret and tildes (^ vs ~)
+
+-initialize git 
+.gitignore
+-create a remote repo on github 
+-push all code to rempte origin 
+-play with routes and route etensions ex. /hello, /test, / xyz
+-order of the routes matter a lot 
+-Install postman App and make a workspace/ collection and Test api call
+-write logic to handle GET, POST , DELETE, PATCH  API call and test them on postman 
